d279d6528171808b5efc53aa316f8a585719760c
Yrol <yrollgayanth@yahoo.com>
2021-07-26 21:34:14 +1000

    Adding profile information

--- client/components/Site/Header.vue
+++ client/components/Site/Header.vue
@@ -65,23 +65,26 @@
         <ul class="flex flex-col lg:flex-row list-none ml-auto">
           <li class="nav-item">
             <div class="px-3 py-2 flex items-center">
-              <nuxt-link
+              <button
                 class="
                   text-xs
                   uppercase
                   font-bold
                   leading-snug
+                  background-transparent
+                  outline-none
+                  focus:outline-none
                   hover:opacity-75
                 "
                 :class="textColor"
-                to="/"
+                @click="showMe()"
               >
                 <i />
                 <font-awesome-icon
                   :icon="['fas', 'user']"
                   class="fa-fw fa-lg mr-1 fa-square fa-w-14"
                 /><span class="inline-block align-middle">me</span>
-              </nuxt-link>
+              </button>
             </div>
           </li>
           <li class="nav-item">
@@ -107,13 +110,79 @@
           </li>
         </ul>
       </div>
+      <Modal :visible="showProfileModal" @close="showProfileModal = false">
+        <div
+          class="
+            bg-white
+            p-3
+            rounded
+            text-center
+            py-8
+            hover:shadow-md
+            cursor-pointer
+          "
+        >
+          <div class="flex justify-center mb-2">
+            <img
+              src="~/assets/images/profile_image.png"
+              class="
+                rounded-full
+                h-40
+                w-40
+                flex
+                items-center
+                border-solid border-4 border-blue-500
+                justify-center
+              "
+            />
+          </div>
+          <h1 class="text-3xl text-gray-700">Yrol Fernando</h1>
+          <span class="text-1xl text-gray-700">Software Developer</span>
+          <div class="mt-3">
+            <a @click="goToSite('https://www.linkedin.com/in/yrol-fernando/')">
+              <font-awesome-icon
+                :icon="['fab', 'linkedin']"
+                class="fa-fw fa-lg mr-1 text-gray-500 fa-square fa-w-14"
+            /></a>
+            <a @click="goToSite('https://github.com/Yrol/')">
+              <font-awesome-icon
+                :icon="['fab', 'github']"
+                class="fa-fw fa-lg mr-1 fa-square text-gray-500 fa-w-14"
+            /></a>
+          </div>
+          <div class="mt-2">
+            <button
+              class="
+                bg-blue-500
+                hover:bg-blue-700
+                text-white
+                w-5/12
+                font-bold
+                py-2
+                px-4
+                rounded-full
+                background-transparent
+                outline-none
+                focus:outline-none
+              "
+              @click="showProfileModal = false"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </Modal>
     </nav>
   </header>
 </template>
 
 <script>
+import Modal from '~/components/Site/Modal';
 export default {
   name: 'SiteHeader',
+  components: {
+    Modal,
+  },
   props: {
     backgroundColor: {
       type: String,
@@ -135,6 +204,7 @@ export default {
   data() {
     return {
       showMenu: false,
+      showProfileModal: false,
     };
   },
   methods: {
@@ -143,6 +213,10 @@ export default {
     },
     goToSite: function (link, target = '_blank') {
       window.open(link, target);
+    },
+    showMe: function () {
+      this.toggleNavbar();
+      this.showProfileModal = true;
     },
   },
 };
