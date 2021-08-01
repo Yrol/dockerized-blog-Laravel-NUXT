e0db8fb9e6e980d91d20bc3ae2f76b4b6e732175
Yrol <yrollgayanth@yahoo.com>
2021-08-01 22:14:05 +1000

    Include MySQL CLI in php docker and include db dump command cron.

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('db:dump')->cron('0 6,13 * * 1,4');
     }
 
     /**
