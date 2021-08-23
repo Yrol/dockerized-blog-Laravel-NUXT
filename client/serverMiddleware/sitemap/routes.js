23717db936e457fd79dd3df7f8301f799dd1ab23
Yrol <yrollgayanth@yahoo.com>
2021-08-23 23:14:53 +1000

    Adding a new Axios instace for the sitemap

--- client/serverMiddleware/sitemap/routes.js
+++ client/serverMiddleware/sitemap/routes.js
@@ -7,41 +7,55 @@ let baseUrl =  process.env.API_URL;
 let siteUrl = process.env.API_URL_BROWSER
 
 router.get('/.xml', async function (req, res, next) {
-  let { data } = await axios.get(`${baseUrl}/api/articles/seo`)
-
   var siteMapCollection = []
 
-  data.data.map(post => {
-
-    let postObj = {
-      loc:`/posts/${post.slug}`,
-      changefreq: 'daily',
-      priority: 1,
-      lastmod: post.updated_at
-    }
-    siteMapCollection.push(postObj)
-
-    let categoryObj = {
-      loc:`/categories/${post.category.slug}`,
-      changefreq: 'daily',
-      priority: 1,
-      lastmod: post.category.updated_at_dates.updated_at_format_1
-    }
-
-    var categoryExist = siteMapCollection.filter(function (category) {
-      return (category.loc == categoryObj.loc);
+  try {
+    const customAxios = axios.create({
+      baseURL: `${baseUrl}/api/`
     });
+    let { data } = await customAxios.get('articles/seo');
 
-    if (categoryExist.length == 0){
-      siteMapCollection.push(categoryObj);
+    data.data.map(post => {
+
+      let postObj = {
+        loc:`/posts/${post.slug}`,
+        changefreq: 'daily',
+        priority: 1,
+        lastmod: post.updated_at
+      }
+      siteMapCollection.push(postObj)
+
+      let categoryObj = {
+        loc:`/categories/${post.category.slug}`,
+        changefreq: 'daily',
+        priority: 1,
+        lastmod: post.category.updated_at_dates.updated_at_format_1
+      }
+
+      var categoryExist = siteMapCollection.filter(function (category) {
+        return (category.loc == categoryObj.loc);
+      });
+
+      if (categoryExist.length == 0){
+        siteMapCollection.push(categoryObj);
+      }
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
     }
-  });
+    console.log(error);
+  }
 
   try {
-      res.setHeader('Content-Type', 'application/xml')
-      res.send(
-        generateSitemap(siteMapCollection, siteUrl)
-      )
+      res.setHeader('Content-Type', 'application/xml');
+      res.send(generateSitemap(siteMapCollection, siteUrl));
   } catch (e) {
     next(e)
   }
